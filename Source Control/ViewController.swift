776e0689d823a862384e86cadac637b11c6ec7c7
Heejeong Cho <cho.h.developer@gmail.com>
2020-03-11 16:23:32 -0300

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Heejeong Cho on 2020-03-11.
+//  Copyright © 2020 Heejeong Cho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
